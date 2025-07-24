486dffef52fceeeb7306810f1d664f64d3927d4d
dingdinglz <1149978856@qq.com>
2025-07-24 17:42:21 +0800

    ✨ feat(cors): 支持跨域

--- server/init.go
+++ server/init.go
@@ -2,12 +2,13 @@ package server
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	fiberlogger "github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
 func InitServer() {
 	Server = fiber.New()
-	Server.Use(fiberlogger.New(), recover.New())
+	Server.Use(fiberlogger.New(), recover.New(), cors.New())
 	AddRoutes()
 }
